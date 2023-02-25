4e27e652a37d5ba0dca11000938705ef91713c5b
Ayush <uniyalayush8532@gmail.com>
2023-02-25 20:06:10 +0530

    program

---
+++ Program_18.cpp
@@ -0,0 +1,69 @@
+// Problem Statement: Given an array of numbers, you need to return the count of reverse pairs. Reverse Pairs are those pairs where i<j and arr[i]>2*arr[j].
+
+// Input: nums = [1,3,2,3,1]
+// Output : 2
+#include <bits/stdc++.h>
+using namespace std;
+
+int Merge(vector<int> &nums, int low, int mid, int high)
+{
+    int total = 0;
+    int j = mid + 1;
+    for (int i = low; i <= mid; i++)
+    {
+        while (j <= high && nums[i] > 2 * (long long)nums[j])
+        {
+            j++;
+        }
+        total += (j - (mid + 1));
+    }
+
+    vector<int> t;
+    int left = low, right = mid + 1;
+
+    while (left <= mid && right <= high)
+    {
+
+        if (nums[left] <= nums[right])
+        {
+            t.push_back(nums[left++]);
+        }
+        else
+        {
+            t.push_back(nums[right++]);
+        }
+    }
+
+    while (left <= mid)
+    {
+        t.push_back(nums[left++]);
+    }
+    while (right <= high)
+    {
+        t.push_back(nums[right++]);
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        nums[i] = t[i - low];
+    }
+    return total;
+}
+int MergeSort(vector<int> &nums, int low, int high)
+{
+
+    if (low >= high)
+        return 0;
+    int mid = (low + high) / 2;
+    int inv = MergeSort(nums, low, mid);
+    inv += MergeSort(nums, mid + 1, high);
+    inv += Merge(nums, low, mid, high);
+    return inv;
+}
+int main()
+{
+    vector<int> nums = {1, 3, 2, 3, 1};
+    int total = MergeSort(nums, 0, nums.size() - 1);
+    cout << total;
+    return 0;
+}
