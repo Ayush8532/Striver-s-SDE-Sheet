86bc20ec92347f0f5484827aadf9f575686429b7
Ayush <uniyalayush8532@gmail.com>
2023-02-27 22:46:16 +0530

    New one

---
+++ Program_20.cpp
@@ -0,0 +1,4 @@
+//Problem Statement: Given an array of N integers, your task is to find unique quads that add up to give a target value. In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
+
+// Input Format: arr[] = [1,0,-1,0,-2,2], target = 0
+//Result : [ [ -2, -1, 1, 2 ], [ -2, 0, 0, 2 ], [ -1, 0, 0, 1 ] ]
